b1a8d5ce6ec79fc3a65e221e75706c22a4643364
akshatha-27 <nayakakshatha27@gmail.com>
2025-02-27 00:40:42 +0530

    adding files

--- server.js
+++ server.js
@@ -5,8 +5,22 @@ http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write("<h2>Hey server started</h2>");
-    res.end();
-  })
-  .listen(port, () => {
-    console.log(`NodeJs Server Started Running On Port ${port}`);
-  });
+    const toDoList = ["learn", "apply things", "succeed"];
+
+    http
+      .createServer((req, res) => {
+        const { method, url } = req;
+        //console.log(method, url);
+        if (url === "/todos") {
+          if (method === "GET") {
+            res.writeHead(200);
+            res.write(toDoList.toString());
+          }
+        }
+        // res.writeHead(200, { "Content-Type": "text/html" });
+        // res.write("<h2>Hey server started</h2>");
+        res.end();
+      })
+      .listen(port, () => {
+        console.log(`NodeJs Server Started Running On Port ${port}`);
+      });
