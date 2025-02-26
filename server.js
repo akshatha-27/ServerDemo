850e9bee3a894e1122fbb4172f67373dc35d55bb
akshatha-27 <nayakakshatha27@gmail.com>
2025-02-26 19:05:20 +0530

    adding files

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const port = 8081;
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h2>Hey server started</h2>");
+    res.end();
+  })
+  .listen(port, () => {
+    console.log(`NodeJs Server Started Running On Port ${port}`);
+  });
