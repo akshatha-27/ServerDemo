85f610a0f358c90db413b6c70ab289bc4f2f9317
akshatha-27 <nayakakshatha27@gmail.com>
2025-02-27 01:08:45 +0530

    changed server.js

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 const http = require("http");
 const port = 8081;
 
+const toDoList = ["learn", "apply things", "succeed"];
+
 http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h2>Hey server started</h2>");
-    const toDoList = ["learn", "apply things", "succeed"];
-
-    http
-      .createServer((req, res) => {
-        const { method, url } = req;
-        //console.log(method, url);
-        if (url === "/todos") {
-          if (method === "GET") {
-            res.writeHead(200);
-            res.write(toDoList.toString());
-          }
-        }
-        // res.writeHead(200, { "Content-Type": "text/html" });
-        // res.write("<h2>Hey server started</h2>");
-        res.end();
-      })
-      .listen(port, () => {
-        console.log(`NodeJs Server Started Running On Port ${port}`);
-      });
+    const { method, url } = req;
+    //console.log(method, url);
+    if (url === "/todos") {
+      if (method === "GET") {
+        res.writeHead(200);
+        res.write(toDoList.toString());
+      }
+    }
+    // res.writeHead(200, { "Content-Type": "text/html" });
+    // res.write("<h2>Hey server started</h2>");
+    res.end();
+  })
+  .listen(port, () => {
+    console.log(`NodeJs Server Started Running On Port ${port}`);
+  });
